5ff9657da6a98c8459f7f75e0d14909799c98658
calindarte <carlosalbertolindartesanto@gmail.com>
2023-08-10 08:44:34 -0500

    image

--- src/assets/components/home/HeaderHome.jsx
+++ src/assets/components/home/HeaderHome.jsx
@@ -3,23 +3,27 @@ import LeftIcon from "../../../assets/components/icons/leftIcon";
 import RightIcon from "../../../assets/components/icons/rightIcon";
 import ArticleHome from "./ArticleHome";
 
+import imgHero1 from "./assets/images/desktop-image-hero-1.jpg"
+import imgHero2 from "./assets/images/desktop-image-hero-2.jpg"
+import imgHero3 from "./assets/images/desktop-image-hero-3.jpg"
+
 const ARRAY_HERO = [
     {
       title: "Discover innovative ways to decorate",
       description: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
-      img_hero: 'src/assets/images/desktop-image-hero-1.jpg',
+      img_hero: {imgHero1},
       id: 1
     },
     {
       title: "We are available all across the globe",
       description: "With stores all over the world, it's easy for you to find furniture for your home or place of business.Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
-      img_hero: 'src/assets/images/desktop-image-hero-2.jpg',
+      img_hero:{imgHero2},
       id: 2
     },
     {
       title: "Manufactured with the best materials",
       description: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
-      img_hero: 'src/assets/images/desktop-image-hero-3.jpg',
+      img_hero: {imgHero3},
       id: 3
     },
   ];
